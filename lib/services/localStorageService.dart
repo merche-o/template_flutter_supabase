aeb290c14ac705f1228a2247549728d29ba37c46
olivier-innosonian <44357691+olivier-innosonian@users.noreply.github.com>
2021-08-12 17:36:37 +0900

    fix image storage

--- lib/services/localStorageService.dart
+++ lib/services/localStorageService.dart
@@ -23,19 +23,19 @@ class LocalStorageService {
     await prefs.remove(key);
   }
 
-  static Future<bool> saveImage(String key, List<int> imageBytes) async {
+  Future<bool> saveImage(String key, List<int> imageBytes) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     String base64Image = base64Encode(imageBytes);
     return prefs.setString(key, base64Image);
   }
 
-  static Future<Image> getImage(String key) async {
+  Future<ImageProvider?> getImage(String key) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     if (prefs.getString(key) == null) {
-      return Image.asset('assets/images/noavatar.jpeg');
+      return null;
     } else {
       Uint8List bytes = base64Decode(prefs.getString(key)!);
-      return Image.memory(bytes);
+      return MemoryImage(bytes);
     }
   }
 }
--- lib/uicomponents/floating_profile_buttonmodel.dart
+++ lib/uicomponents/floating_profile_buttonmodel.dart
@@ -8,13 +8,16 @@ import 'package:stacked/stacked.dart';
 import 'package:template_flutter_supabase/app/app.locator.dart';
 import 'package:template_flutter_supabase/config/initSupabase.dart';
 import 'package:template_flutter_supabase/services/authenticationService.dart';
+import 'package:template_flutter_supabase/services/localStorageService.dart';
 
 class FloatingProfileButtonModel extends BaseViewModel {
   var _logger = Logger();
   final _authService = locator<AuthenticationService>();
   final _navService = locator<NavigationService>();
-  ImageProvider _image = AssetImage('assets/images/noavatar.jpeg');
-  ImageProvider get image => _image;
+  final _localStoreService = locator<LocalStorageService>();
+
+  ImageProvider? _image = AssetImage('assets/images/noavatar.jpeg');
+  ImageProvider? get image => _image;
 
   FloatingProfileButtonModel() {
     returnImage();
@@ -24,20 +27,21 @@ class FloatingProfileButtonModel extends BaseViewModel {
     String? imageUrl = _authService.user?.avatar_url;
     if (imageUrl == null) {
       _image = AssetImage('assets/images/noavatar.jpeg');
-      notifyListeners();
     } else {
-      final response =
-          await supabase.storage.from('avatars').download(imageUrl);
-      if (response.data != null) {
-        _image = MemoryImage(response.data!);
-        _image.toString();
-
-        notifyListeners();
-      } else {
-        _image = AssetImage('assets/images/noavatar.jpeg');
-        notifyListeners();
+      _image = await _localStoreService.getImage("avatar");
+      if (_image == null) {
+        final response =
+            await supabase.storage.from('avatars').download(imageUrl);
+        if (response.data != null) {
+          _image = MemoryImage(response.data!);
+          await _localStoreService.saveImage("avatar", response.data!);
+        } else {
+          _image = AssetImage('assets/images/noavatar.jpeg');
+        }
       }
     }
+    notifyListeners();
+
     return true;
   }
 
